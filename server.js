20e3ee0a84b2255c5a2536ce67710df5f2414c96
Krishna Rohith <krishnarohith417@gmail.com>
2026-08-07 22:50:47 +0530

    Add public directory and vercel.json outputDirectory for Vercel static deployment

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname)));
 
 // Real Live SMTP Mailer Endpoint
